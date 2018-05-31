40b1b741b6e84b75c181c090bf79a13d0eb64e87
Cherepadla <dmitriybilli@yandex.com>
2018-05-31 17:09:54 +0300

    2

--- Head First/get_number.rb
+++ Head First/get_number.rb
@@ -13,7 +13,28 @@ puts "Угадаешь ?"
 target = rand(100) + 1
 #Отслеживание количества попыток
 num_guesses = 0 # Количество попыток
+#Признак продолжения игры
+guessed_it = false
+
 puts "Тебе осталось #{10 - num_guesses} попыток" 
 print "Введи число: "
 guess = gets.to_i
-#dsfasdf
+
+#Сравнение введенного числа с заданным
+#и вывод соответствующего значения
+if guess < target
+    puts "Ой, ваше число слишком мало."
+elsif guess > target
+    puts "Ой, ваше число слишком велико."
+elsif guess == target
+    puts "Хорошая работа, #{name} !"
+    puts "Ты угадал мое число в #{num_guesses} ходов !"
+    guessed_it = true
+end
+# Если попыток не осталось, сообщить загадочное число
+if not guessed_it
+    puts "Извините. Вы не угадали мое число. (Это было #{target}.)"
+end
+
+
+
