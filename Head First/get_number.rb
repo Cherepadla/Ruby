51253695a53d0a10f0d4a7ce3620205b830a5690
Cherepadla <dmitriybilli@yandex.com>
2018-05-31 09:58:38 +0300

    Initial commit

---
+++ Head First/get_number.rb
@@ -0,0 +1,15 @@
+# Get My number Game
+# Written by: My
+
+puts "Welcome to 'Get My number!'"
+# Получение имени игрока и вывод приветствия
+print "Как вас зовут ? "
+input = gets
+name = input.chomp
+puts "Приветствую, #{name} !"
+# Сохранение случайного числа
+puts "Я загадал число от 0 до 100. "
+puts "Угадаешь ?"
+target = rand(100) + 1
+num_guesses = 0 # Количество попыток
+
---
+++ hello.rb
@@ -0,0 +1,8 @@
+#Давайте поприветствуем Матца
+
+puts "Hello Matzzzzz!"
+puts "Hello, " << "Matz !" * 3
+
+5.times {print "Yello Mutzz"}
+
+puts "Hey Mutz, I'm running " + `ruby -v`
