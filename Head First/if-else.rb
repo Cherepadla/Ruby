7b6d20262deb46b048b36dfc09860ec579814a68
Cherepadla <dmitriybilli@yandex.com>
2018-06-01 17:10:37 +0300

    if-else file

---
+++ Head First/if-else.rb
@@ -0,0 +1,12 @@
+puts "Введите число"
+input = gets.to_i # Приводим вводимое значение к числовому целому (integer)
+
+if input > 10 
+    puts "Ваше число больше 10"
+elsif input == 10
+    puts "Ваше число равно 10"
+elsif 
+    puts "Ваше число меньше 10"
+    
+end
+print input," ", input.class
